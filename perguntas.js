0b96ace0d30643710f25f05bcff403ba9c235480
wzgabz <navarro.menezes@escola.pr.gov.br>
2025-08-21 08:23:14 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Música',
+    'Qual o artista musical mais ouvido do Spotfy Brasil?',
+    'O artista mais ouvido no Spotify Brasil, em 2024, é a dupla sertaneja Henrique & Juliano'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Animais',
+    'O que é considerdo um inseto?',
+    'Corpo dividido em três partes: cabeça, tórax e abdome; um par de antenas; três pares de pernas; asas'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Espaço',
+    'Qual o tamanho do sol?',
+    '1.392.700 km'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Terra',
+    'O que é que provoca o vulcão?',
+    'Acumulação de vapor e gases sob elevadas pressões'
 )
